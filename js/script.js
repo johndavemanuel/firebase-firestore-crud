6adf88bf4cbc9f9de52ce41f030eb88880c28410
johndavemanuel <jdmanuel242@gmail.com>
2019-08-28 11:30:41 +0800

    center modal and add backdrop

--- js/script.js
+++ js/script.js
@@ -215,3 +215,22 @@ $(document).ready(function () {
         });
     });
 });
+(function ($) {
+    "use strict";
+
+    function centerModal() {
+        $(this).css('display', 'block');
+        var $dialog = $(this).find(".modal-dialog"),
+            offset = ($(window).height() - $dialog.height()) / 2,
+            bottomMargin = parseInt($dialog.css('marginBottom'), 10);
+
+        // Make sure you don't hide the top part of the modal w/ a negative margin if it's longer than the screen height, and keep the margin equal to the bottom margin of the modal
+        if (offset < bottomMargin) offset = bottomMargin;
+        $dialog.css("margin-top", offset);
+    }
+
+    $(document).on('show.bs.modal', '.modal', centerModal);
+    $(window).on("resize", function () {
+        $('.modal:visible').each(centerModal);
+    });
+}(jQuery));
